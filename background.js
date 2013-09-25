b58f5c9422178a2835586daa4a384dd16c4fcfc1
Tom Blackmore <tablackmore@gmail.com>
2013-09-25 23:47:02 +0200

    initial commit

---
+++ background.js
@@ -0,0 +1,34 @@
+var activate = false;
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.type === "active") {
+        sendResponse({
+            active: activate
+        });
+    }
+});
+
+function updateIcon() {
+    if (activate) {
+        chrome.browserAction.setIcon({
+            path: "icon2.png"
+        });
+    } else {
+        chrome.browserAction.setIcon({
+            path: "icon1.png"
+        });
+    }
+}
+chrome.browserAction.onClicked.addListener(function () {
+    "use strict";
+    activate = !activate;
+    updateIcon();
+    chrome.tabs.query({
+        //active: true
+    }, function (tabs) {
+        for (var i = 0; i < tabs.length; i = i + 1) {
+            chrome.tabs.sendMessage(tabs[i].id, {
+                active: activate
+            }, function (response) {});
+        }
+    });
+});
---
+++ myscript.js
@@ -0,0 +1,54 @@
+/*jslint browser:true */
+/*global chrome: false,NodeFilter: false, alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
+(function () {
+    "use strict";
+    var wickedWords = ["great", "awesome", "incredible", "wonderful", "amazing", "amasing", "lovely", "beautiful", "pretty"],
+        wickedestWords = ["best", "nicest"];
+
+    function wickedise(text) {
+        var i;
+        if (typeof text === "string") {
+            for (i = 0; i < wickedWords.length; i = i + 1) {
+                text = text.replace(new RegExp("an " + wickedWords[i], 'gi'), "a wicked");
+                text = text.replace(new RegExp(wickedWords[i], 'g'), "wicked");
+                text = text.replace(new RegExp(wickedWords[i].toUpperCase(), 'g'), "WICKED");
+                text = text.replace(new RegExp(wickedWords[i][0].toUpperCase() + wickedWords[i].substr(1, wickedWords[i].length), 'g'), "Wicked");
+            }
+            for (i = 0; i < wickedestWords.length; i = i + 1) {
+                text = text.replace(new RegExp(wickedestWords[i], 'g'), "wickedest");
+                text = text.replace(new RegExp(wickedestWords[i].toUpperCase(), 'g'), "WICKEDEST");
+                text = text.replace(new RegExp(wickedestWords[i][0].toUpperCase() + wickedestWords[i].substr(1, wickedestWords[i].length), 'g'), "Wickedest");
+            }
+        }
+        return text;
+    }
+    //Walk through all the text nodes in the body and wickedise their text
+
+    function wickedizer() {
+        var walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false),
+            node;
+        while (node = walker.nextNode()) {
+            node.nodeValue = wickedise(node.nodeValue);
+        }
+    }
+    chrome.runtime.sendMessage({
+        type: "active"
+    }, function (response) {
+        if (response.active) {
+            wickedizer();
+        }
+    });
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        if (request.active) {
+            wickedizer();
+            sendResponse({
+                activated: true
+            });
+        } else {
+            location.reload();
+            sendResponse({
+                activated: false
+            });
+        }
+    });
+}());
