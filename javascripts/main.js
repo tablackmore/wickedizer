5b4cdded95db4a766ac2cc57888254a7ad065aef
Tom Blackmore <tablackmore@gmail.com>
2013-09-30 21:05:45 +0200

    Proper wicked support added

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,10 @@
   document.addEventListener("DOMContentLoaded", function () {
-      var wickedizerButton = document.querySelector("input"),
-          textArea = document.querySelector("textArea"),
-          wickedOutputContainer = document.querySelector("#wickedOutput"),
-          wickedOutput = document.querySelector("#wickedOutput p");
-      wickedizerButton.onclick = function () {
-          wickedOutput.innerText = wickedizer.textReplacer(textArea.value);
-          wickedOutputContainer.style.display = "block";
-      };
+    var wickedizerButton = document.querySelector("input"),
+      textArea = document.querySelector("textArea"),
+      wickedOutputContainer = document.querySelector("#wickedOutput"),
+      wickedOutput = document.querySelector("#wickedOutput p");
+    wickedizerButton.onclick = function () {
+      wickedOutput.innerText = hatchet.wickedizer(textArea.value);
+      wickedOutputContainer.style.display = "block";
+    };
   });
--- javascripts/wickedizer.js
+++ javascripts/wickedizer.js
@@ -1,51 +1,77 @@
-/* wickedizer - v0.0.1 - 2013-09-26 */
-var wickedizer = {};
-wickedizer.data = {};
-wickedizer.data.wickedWords = ["great", "awesome", "incredible", "brilliant", "wonderful", "amazing", "amasing", "lovely", "beautiful", "pretty", "splendid"];
-wickedizer.data.wickedestWords = ["best", "nicest"];
-wickedizer.textReplacer = function (text) {
+/* wickedizer - v0.0.1 - 2013-09-30 */
+var hatchet = {};
+hatchet.data = {};
+hatchet.data.wickedizer = {};
+hatchet.data.wickedizer.wickedWords = ["great", "awesome", "incredible", "wonderful", "splendid", "awesome", "incredible", "amazing", "amasing"];
+hatchet.data.wickedizer.wickedestWords = ["best", "nicest"];
+hatchet.data.wickedizer.properWickedWords = ["awesome", "incredible", "amazing", "amasing"];
+hatchet.wickedizer = (function () {
     "use strict";
-    if (typeof text === "string") {
-        text = text.replace(new RegExp(wickedizer.data.wickedWords.join("|") + "|an " + wickedizer.data.wickedWords.join("|an "), 'gi'), function (match) {
-            var output = "wicked";
-            if (match.match(new RegExp("an ", "gi"))) {
-                output = "a " + output;
-            }
-            // TitleCase
-            if (match.match(new RegExp("([A-Z]{1}[a-z]+)", "g"))) {
-                output = output.replace(/\w\S*/g, function (txt) {
-                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    var regExpMostWicked = "\\bmost " + hatchet.data.wickedizer.wickedWords.join("\\b|\\bmost ") + "\\b",
+        regExpWickedest = new RegExp(regExpMostWicked + "|" + hatchet.data.wickedizer.wickedestWords.join("|"), 'gi'),
+        regExpProperWicked = new RegExp("\\b" + hatchet.data.wickedizer.properWickedWords.join("\\b|\\b") + "\\b|\\ban " + hatchet.data.wickedizer.wickedWords.join("\\b|\\ban ") + "\\b", 'gi'),
+        regExpWicked = new RegExp(hatchet.data.wickedizer.wickedWords.join("|") + "|an " + hatchet.data.wickedizer.wickedWords.join("|an "), 'gi'),
+        wickedize = function (text) {
+            if (typeof text === "string") {
+                text = text.replace(regExpWickedest, function (match) {
+                    // TitleCase
+                    if (match.match(new RegExp("([A-Z]{1}[a-z]+)", "g"))) {
+                        return "Wickedest";
+                    }
+                    // UpperCase
+                    if (match.match(new RegExp("([A-Z]{1}[A-Z]+)", "g"))) {
+                        return "WICKEDEST";
+                    }
+                    // LowerCase
+                    return "wickedest";
+                });
+                text = text.replace(regExpProperWicked, function (match) {
+                    var output = "proper wicked";
+                    if (match.match(new RegExp("an ", "gi"))) {
+                        output = "a " + output;
+                    }
+                    // TitleCase
+                    if (match.match(new RegExp("([A-Z]{1}[a-z]+)", "g"))) {
+                        output = output.replace(/\w\S*/g, function (txt) {
+                            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                        });
+                    }
+                    // UpperCase
+                    if (match.match(new RegExp("([A-Z]{1}[A-Z]+)", "g"))) {
+                        output = output.toUpperCase();
+                    }
+                    return output;
+                });
+                text = text.replace(regExpWicked, function (match) {
+                    var output = "wicked";
+                    if (match.match(new RegExp("an ", "gi"))) {
+                        output = "a " + output;
+                    }
+                    // TitleCase
+                    if (match.match(new RegExp("([A-Z]{1}[a-z]+)", "g"))) {
+                        output = output.replace(/\w\S*/g, function (txt) {
+                            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                        });
+                    }
+                    // UpperCase
+                    if (match.match(new RegExp("([A-Z]{1}[A-Z]+)", "g"))) {
+                        output = output.toUpperCase();
+                    }
+                    return output;
                 });
             }
-            // UpperCase
-            if (match.match(new RegExp("([A-Z]{1}[A-Z]+)", "g"))) {
-                output = output.toUpperCase();
-            }
-            return output;
-        });
-        text = text.replace(new RegExp(wickedizer.data.wickedestWords.join("|"), 'gi'), function (match) {
-            // TitleCase
-            if (match.match(new RegExp("([A-Z]{1}[a-z]+)", "g"))) {
-                return "Wickedest";
-            }
-            // UpperCase
-            if (match.match(new RegExp("([A-Z]{1}[A-Z]+)", "g"))) {
-                return "WICKEDEST";
-            }
-            // LowerCase
-            return "wickedest";
-        });
-    }
-    return text;
-};
-wickedizer.htmlTextNodeWalker = function () {
+            return text;
+        };
+    return wickedize;
+}());
+hatchet.htmlTextNodeWalker = function () {
     var walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false),
         node;
     while (node = walker.nextNode()) {
-        node.nodeValue = wickedizer.textReplacer(node.nodeValue);
+        node.nodeValue = hatchet.wickedizer(node.nodeValue);
     }
 };
-wickedizer.showActiveSplash = function () { //not in an iframe
+hatchet.showActiveSplash = function () { //not in an iframe
     if (window.self === window.top) {
         var div = document.createElement("div");
         div.style.position = "fixed";
@@ -53,12 +79,12 @@ wickedizer.showActiveSplash = function () { //not in an iframe
         div.style.height = "30px";
         div.style.fontSize = "10px";
         div.style.color = "#777";
-        div.innerHTML = "WICKEDIZER<br/>ACTIVE";
         div.style.backgroundColor = "red";
         div.style.bottom = "0px";
         div.style.left = "0px";
         div.style.padding = "4px";
         div.style.zIndex = "3147483647";
+        div.innerHTML = "WICKEDIZER<br/>ACTIVE";
         document.querySelector("body").appendChild(div);
     }
 };
